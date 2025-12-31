9699d001d6bb87b496b26160be7e0340031f4ee5
Gabriel Cohen <gabriel.c@layerxsecurity.com>
2025-12-31 16:10:42 +0200

    added gitsigns and neotree and some updates to treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,29 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    lazy = false,
-    build = ':TSUpdate'
+	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		local parsers = {
+			"lua",
+			"vim",
+			"vimdoc",
+			"query",
+			"bash",
+			"python",
+			"javascript",
+			"typescript",
+			"tsx",
+			"json",
+			"yaml",
+			"toml",
+			"markdown",
+			"markdown_inline",
+			"svelte",
+			"sql",
+			"terraform",
+			"hcl",
+			"rust",
+		}
+		require("nvim-treesitter").install(parsers)
+	end,
 }
